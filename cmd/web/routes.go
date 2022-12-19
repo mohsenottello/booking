1bffea92a3388b881a58c28510846ebe160375e1
vboxuser <vboxuser@ottelllo.myguest.virtualbox.org>
2022-12-19 20:04:41 +0100

    Initial commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/ottello/pkg/config"
+	"github.com/ottello/pkg/handlers"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurve)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
